b72741ac0c77a22e03df033a35933b6bef5f4ad0
Gautham Arun <159702372+gauthamarun@users.noreply.github.com>
2026-07-11 15:25:50 +0530

    Container with most water added

---
+++ array/container-most-water.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxArea(self, height):
+        l,r = 0, len(height)-1
+        max_area = 0
+        while l<r:
+            if height[l]<height[r]:
+                curr_area = height[l]*(r-l)
+                max_area = max(curr_area, max_area)
+                l+=1
+            elif height[l]>height[r]:
+                curr_area = height[r]*(r-l)
+                max_area = max(curr_area, max_area)
+                r-=1
+            else:
+                curr_area = height[r]* (r-l)
+                max_area = max(curr_area, max_area)  
+                l+=1
+                r-=1        
+        return max_area
