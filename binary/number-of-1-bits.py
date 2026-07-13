20f360bb78e4e45487eccf3de2be498c8dff81dd
Gautham Arun <159702372+gauthamarun@users.noreply.github.com>
2026-07-13 17:55:11 +0530

    Modified number of 1 bits using n&(n-1) method

--- binary/number-of-1-bits.py
+++ binary/number-of-1-bits.py
@@ -1,8 +1,7 @@
 class Solution(object):
     def hammingWeight(self, n):
-        binary = bin(n)[2:]
-        result = 0
-        for ch in binary:
-            if ch=='1':
-                result+=1
-        return result        
+        count = 0
+        while n:
+            n = n&(n-1)
+            count+=1
+        return count          
