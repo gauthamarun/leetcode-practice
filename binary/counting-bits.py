0467c5ef1553804dd64140452116a8d327e6f51e
Gautham Arun <159702372+gauthamarun@users.noreply.github.com>
2026-07-14 23:01:38 +0530

    Updated with DP Solution

--- binary/counting-bits.py
+++ binary/counting-bits.py
@@ -1,7 +1,8 @@
+#dp solution
 class Solution(object):
     def countBits(self, n):
         dp = [0]*(n+1)
         for i in range(1,n+1):
             x = i&(i-1)
             dp[i] = dp[x]+1
-        return dp          
+        return dp 
