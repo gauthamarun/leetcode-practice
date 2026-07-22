6bfca61439c3fbe575f997af11bbbe29aafbca6f
Gautham Arun <159702372+gauthamarun@users.noreply.github.com>
2026-07-22 15:23:47 +0530

    Reverse bits added

---
+++ binary/reverse-bits.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def reverseBits(self, n):
+        result = 0
+        for i in range(32):
+            bit = (n>>i) & 1
+            result = result | (bit<<(31-i))
+        return result    
