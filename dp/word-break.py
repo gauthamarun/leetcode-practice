b5ad4cf38d38bf993afca887b6c4489a750dd7da
Gautham Arun <159702372+gauthamarun@users.noreply.github.com>
2026-07-26 17:08:04 +0530

    Word Break Added

---
+++ dp/word-break.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        wordDict = set(wordDict)
+        dp = [False]* (len(s)+1)
+        dp[0] = True
+        for i in range(1,len(s)+1):
+            for j in range(i):
+                if dp[j]==True and s[j:i] in wordDict:
+                    dp[i] = True
+        return dp[len(s)]                        
