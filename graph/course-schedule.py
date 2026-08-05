f2f9302763e7d3d245d8501d877453ae8046772a
Gautham Arun <159702372+gauthamarun@users.noreply.github.com>
2026-08-05 23:35:51 +0530

    Course Schedule added

---
+++ graph/course-schedule.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        g = defaultdict(list)
+        courses = prerequisites
+        for a,b in courses:
+            g[a].append(b)
+        UNVISITED = 0
+        VISITING = 1
+        VISITED = 2
+        states = [UNVISITED]*numCourses
+
+        def dfs(node):
+            state = states[node]
+            if state == VISITED: return True
+            elif state == VISITING: return False
+
+            states[node] = VISITING
+
+            for nei in g[node]:
+                if not dfs(nei):
+                    return False
+
+            states[node] = VISITED
+            return True        
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True                
+        
