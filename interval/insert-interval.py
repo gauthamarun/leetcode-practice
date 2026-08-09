ec682124a400a9474642176e9fc5e5884d5c70c2
Gautham Arun <159702372+gauthamarun@users.noreply.github.com>
2026-08-09 23:10:20 +0530

    Instert Interval Added

---
+++ interval/insert-interval.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        result = []
+        for i in range(len(intervals)):
+            if newInterval[1]<intervals[i][0]:
+                result.append(newInterval)
+                return result + intervals[i:]
+            elif newInterval[0]>intervals[i][1]:
+                result.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]        
+        result.append(newInterval)    
+        return result    
