8e07d496365f5f42466d53a156dd07b13a066d11
Gautham Arun <159702372+gauthamarun@users.noreply.github.com>
2026-08-11 13:06:19 +0530

    Merge k sorted linked list added

---
+++ linked-list/merge-k-sorted-linked-list.py
@@ -0,0 +1,28 @@
+import heapq
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        heap = []
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap,(node.val,i,node))
+        D = ListNode()
+        curr = D
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            curr.next = node
+            curr = node
+            node = node.next
+            if node:
+                heapq.heappush(heap,(node.val,i,node))
+        return D.next           
+
+
+                
+
+        
